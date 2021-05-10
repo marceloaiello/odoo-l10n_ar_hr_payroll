7b278cba38171799ec176f12a49a305928e3f080
nicolasrsande <nicolasrsande@gmail.com>
2021-05-10 13:56:30 -0300

    push first version of hr_labor_union

---
+++ l10n_ar_hr_contract_labor_union/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Argentinian Localization - Labor Unions',
+    'version': '13.0.1.0',
+    'description': 'Adds labor union management to hr_contract and hr_payroll',
+    'summary': 'Adds labor union management to hr_contract and hr_payroll',
+    'author': 'Nimarosa',
+    'website': 'www.nimarosa.com',
+    'license': 'LGPL-3',
+    'category': 'Human Resources',
+    'depends': [
+        'base',
+        'hr_contract',
+        'hr'
+    ],
+    'data': [
+        ''
+    ],
+    'demo': [
+        ''
+    ],
+    'auto_install': True,
+    'application': True,
+}
---
+++ l10n_ar_hr_contract_labor_union/models/__init__.py
@@ -0,0 +1,1 @@
+import . from hr_labor_union
