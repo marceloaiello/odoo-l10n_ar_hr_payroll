fb2ef846b1ec238a691be18daa9b3404e89ab63a
nicolasrsande <nicolasrsande@gmail.com>
2021-05-10 16:04:59 -0300

    fix another typo

--- l10n_ar_hr_contract_labor_union/models/__init__.py
+++ l10n_ar_hr_contract_labor_union/models/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (C) 2021 Nimarosa (Nicolas Rodriguez) (<nicolasrsande@gmail.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-form . import hr_labor_union
+from . import hr_labor_union
