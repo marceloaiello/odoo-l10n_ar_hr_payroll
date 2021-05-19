50362c9064f313c996fa8eacceb2657eeb3d01df
nicolasrsande <nicolasrsande@gmail.com>
2021-05-19 15:57:47 -0300

    try with dependencies

--- hr_employee_ux/__manifest__.py
+++ hr_employee_ux/__manifest__.py
@@ -11,7 +11,26 @@
     'license': 'LGPL-3',
     'category': 'Human Resources',
     'depends': [
-        'hr_employee'
+        'base',
+        'hr',
+        'hr_attendance',
+        'hr_attendance_autoclose',
+        'hr_attendance_geolocation',
+        'hr_attendance_reason',
+        'hr_attendance_report_theoretical_time',
+        'hr_contract',
+        'hr_contract_rate',
+        'hr_employee_age',
+        'hr_employee_calendar_planning',
+        'hr_employee_document',
+        'hr_employee_identification',
+        'hr_employee_medical_examination',
+        'hr_employee_ppe',
+        'hr_employee_relative',
+        'hr_employee_service',
+        'hr_employee_service_contract',
+        'hr_org_chart',
+        'hr_org_chart_overview'
     ],
     'data': [
         'views/hr_employee.xml'
