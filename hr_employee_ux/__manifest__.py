61181965061f7d950da4d79a1a683025197efde9
nicolasrsande <nicolasrsande@gmail.com>
2021-05-19 01:54:10 -0300

    release hr_employee_ux tweaks

---
+++ hr_employee_ux/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2021 Nimarosa (Nicolas Rodriguez) (<nicolasrsande@gmail.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'HR Employee UX Customizations',
+    'version': '13.0.1.0',
+    'description': 'This module adds ux customizations to the hr_employee module and submodules to make forms and views more flexible.',
+    'summary': 'Adds modifications to the ux of the hr_employee module.',
+    'author': 'Nimarosa',
+    'website': 'www.nimarosa.com.ar',
+    'license': 'LGPL-3',
+    'category': 'Human Resources',
+    'depends': [
+        'hr',
+        'hr_employee'
+    ],
+    'data': [
+        'views/hr_employee.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
