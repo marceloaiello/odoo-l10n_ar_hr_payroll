e2cb85dada8acc5607af9d4e83e0dc97f99344c6
nicolasrsande <nicolasrsande@gmail.com>
2021-05-30 20:41:19 -0300

    update manifest

--- hr_contract_labor_union/__manifest__.py
+++ hr_contract_labor_union/__manifest__.py
@@ -16,7 +16,6 @@
     'data': [
         'security/ir.model.access.csv',
         'views/hr_labor_union.xml',
-        'views/hr_labor_union_svco_value.xml',
         'views/hr_labor_union_category.xml',
         'views/hr_contract.xml'
     ],
