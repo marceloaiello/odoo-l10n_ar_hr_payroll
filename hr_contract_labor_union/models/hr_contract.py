04a8cd9fa7f665770d4a9f5fc58483b60e02dabb
nicolasrsande <nicolasrsande@gmail.com>
2021-05-31 11:57:39 -0300

    merge domain in view

--- hr_contract_labor_union/models/hr_contract.py
+++ hr_contract_labor_union/models/hr_contract.py
@@ -9,3 +9,11 @@ class HrContract(models.Model):
     cct_id = fields.Many2one(comodel_name='hr.labor_union', string='C.C.T / Sindicato')
     cct_category_id = fields.Many2one(comodel_name='hr.labor_union.category', string='Categoria C.C.T')
 
+    @api.onchange(cct_category_id, cct_id, aplica_cct)
+    def on_change_cct_category_id(self):
+        for record in self:
+            if record.cct_category_id is not None & record.aplica_cct == True:
+                record.amount = record.cct_category_id.current_value
+            else:
+                break
+
