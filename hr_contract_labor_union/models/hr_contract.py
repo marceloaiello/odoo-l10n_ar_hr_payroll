8dcf59512bf1117f18295a490cfd20ca8ff96f2e
nicolasrsande <nicolasrsande@gmail.com>
2021-05-31 11:35:28 -0300

    delete autoupdate of amount, will think in another solution

--- hr_contract_labor_union/models/hr_contract.py
+++ hr_contract_labor_union/models/hr_contract.py
@@ -9,11 +9,3 @@ class HrContract(models.Model):
     cct_id = fields.Many2one(comodel_name='hr.labor_union', string='C.C.T / Sindicato')
     cct_category_id = fields.Many2one(comodel_name='hr.labor_union.category', string='Categoria C.C.T', domain=[('labor_union_id', '=', cct_id)])
 
-    @api.onchange(cct_category_id, cct_id, aplica_cct)
-    def on_change_cct_category_id(self):
-        for record in self:
-            if record.cct_category_id is not None & record.aplica_cct == True:
-                record.amount = record.cct_category_id.current_value
-            else:
-                break
-
