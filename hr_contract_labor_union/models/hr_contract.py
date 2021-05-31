5cb1f95c2f4915fa60756661dd761a153aeece03
nicolasrsande <nicolasrsande@gmail.com>
2021-05-31 15:29:13 -0300

    other aproach

--- hr_contract_labor_union/models/hr_contract.py
+++ hr_contract_labor_union/models/hr_contract.py
@@ -7,16 +7,13 @@ class HrContract(models.Model):
     _inherit = 'hr.contract'
     aplica_cct = fields.Boolean(string='Aplica C.C.T / Convenio?')
     cct_id = fields.Many2one(comodel_name='hr.labor_union', string='C.C.T / Sindicato')
-    cct_category_id = fields.Many2one(comodel_name='hr.labor_union.category', string='Categoria C.C.T', onchange='on_change_cct_category_id')
-    cct_value = fields.Monetary(name="Importe Categoria", readonly=True, options="{'currency_field': 'currency_id'}")
-    currency_id = fields.Many2one('res.currency', string='Moneda', required=True,
-        default=lambda self: self.env.user.company_id.currency_id.id)
+    cct_category_id = fields.Many2one(comodel_name='hr.labor_union.category', string='Categoria C.C.T', onchange='_on_change_cct_category_id')
+    cct_value = fields.Many2one(related="cct_category_id.current_value", string="Importe Categoria")
 
-    @api.depends('cct_category_id')
-    @api.onchange(cct_category_id, aplica_cct)
-    def on_change_cct_category_id(self):
-        if self.cct_category_id:
-            self.cct_value = self.cct_category_id.current_value
-            self.amount = self.cct_category_id.current_value
+    @api.depends('cct_category_id', 'cct_value')
+    @api.onchange(cct_category_id)
+    def _on_change_cct_category_id(self):
+        if self.cct_value > 0:
+            self.amount = self.cct_value
         else:
-            self.cct_value = 0
+            self.amount = self.amount
