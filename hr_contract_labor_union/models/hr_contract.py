c7f33a16995fa0261b6f0ab0039832e635cf3fd8
nicolasrsande <nicolasrsande@gmail.com>
2021-05-31 15:21:41 -0300

    will try this aproach

--- hr_contract_labor_union/models/hr_contract.py
+++ hr_contract_labor_union/models/hr_contract.py
@@ -8,10 +8,15 @@ class HrContract(models.Model):
     aplica_cct = fields.Boolean(string='Aplica C.C.T / Convenio?')
     cct_id = fields.Many2one(comodel_name='hr.labor_union', string='C.C.T / Sindicato')
     cct_category_id = fields.Many2one(comodel_name='hr.labor_union.category', string='Categoria C.C.T', onchange='on_change_cct_category_id')
+    cct_value = fields.Monetary(name="Importe Categoria", readonly=True, options="{'currency_field': 'currency_id'}")
+    currency_id = fields.Many2one('res.currency', string='Moneda', required=True,
+        default=lambda self: self.env.user.company_id.currency_id.id)
 
-    @api.onchange(cct_category_id)
+    @api.depends('cct_category_id')
+    @api.onchange(cct_category_id, aplica_cct)
     def on_change_cct_category_id(self):
-        if self.cct_category_id & self.aplica_cct == True:
+        if self.cct_category_id:
+            self.cct_value = self.cct_category_id.current_value
             self.amount = self.cct_category_id.current_value
         else:
-            self.amount = self.amount
+            self.cct_value = 0
