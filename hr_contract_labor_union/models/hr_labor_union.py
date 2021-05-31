f21fee7106750097ac11b588bb39c69e1c2bd0b9
nicolasrsande <nicolasrsande@gmail.com>
2021-05-31 11:05:01 -0300

    merge new changes

--- hr_contract_labor_union/models/hr_labor_union.py
+++ hr_contract_labor_union/models/hr_labor_union.py
@@ -8,7 +8,7 @@ class HrLaborUnion(models.Model):
     _inherit = 'mail.thread' # Add Chatter Tracking
     _description = 'Union Laboral / Sindicatos'
 
-    name = fields.Char(string='C.C.T / Sindicato', track_visibility='onchange')
+    name = fields.Char(string='C.C.T / Sindicato', compute="_compute_name")
     sindicato = fields.Char(string='Sindicato', track_visibility='onchange')
     convenio = fields.Char(string='Codigo C.C.T / RAMA', track_visibility='onchange')
     cct_categories = fields.One2many(comodel_name='hr.labor_union.category',inverse_name='labor_union_id',
@@ -18,6 +18,11 @@ class HrLaborUnion(models.Model):
     svco_current_value = fields.Float(compute='_compute_svco_current_value', string='Valor Actual (SVCO)')
     company_id = fields.Many2one('res.company', string='Empresa', required=True,
         default=lambda self: self.env.user.company_id)
+
+    @api.depends('sindicato', 'convenio')
+    def _compute_name(self):
+        for record in self:
+            record.name = "( " + record.convenio + " ) " + record.sindicato
 
     @api.depends('cct_svco_values')
     def _compute_svco_current_value(self):
