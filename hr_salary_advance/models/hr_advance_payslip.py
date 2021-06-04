7204f96f5ef9ed07f977686f43746309a7926276
nicolasrsande <nicolasrsande@gmail.com>
2021-06-04 19:15:48 -0300

    push new changes (unfinished)

--- hr_salary_advance/models/hr_advance_payslip.py
+++ hr_salary_advance/models/hr_advance_payslip.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
+# Copyright (C) 2021 Nimarosa (Nicolas Rodriguez) (<nicolasrsande@gmail.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from datetime import datetime
 from odoo import models
-
-
 class SalaryRuleInput(models.Model):
     _inherit = 'hr.payslip'
 
--- hr_salary_advance/models/salary_advance.py
+++ hr_salary_advance/models/salary_advance.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
+# Copyright (C) 2021 Nimarosa (Nicolas Rodriguez) (<nicolasrsande@gmail.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 import time
 from datetime import datetime
 from odoo import fields, models, api, _
