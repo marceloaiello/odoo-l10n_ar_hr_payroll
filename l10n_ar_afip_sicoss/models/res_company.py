2743d5d208c914d1eeb28dcd525f2324545e84b0
nicolasrsande <nicolasrsande@gmail.com>
2021-06-27 03:27:34 -0300

    fix dict

--- l10n_ar_afip_sicoss/models/res_company.py
+++ l10n_ar_afip_sicoss/models/res_company.py
@@ -7,13 +7,13 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     afip_sicoss_type = fields.Selection([
-        ('0', 'Administracion Publica',
-         '1', 'Dto. 814/01, art. 2, inc. b).',
-         '2', 'Servicios eventuales. Dto. 814/01, art. 2, inc. b).',
-         '3', 'Provincias u otros.',
-         '4', 'Dto. 814/01, art. 2, inc. a).',
-         '5', 'Servicios eventuales. Dto. 814/01, art. 2, inc. a).',
-         '6', 'Dto. 814/01, art. 2, inc. b). Provincias. Ley 22.016.',
-         '7', 'Colegios privados.')
+        ('0', 'Administracion Publica'),
+        ('1', 'Dto. 814/01, art. 2, inc. b).'),
+        ('2', 'Servicios eventuales. Dto. 814/01, art. 2, inc. b).'),
+        ('3', 'Provincias u otros.'),
+        ('4', 'Dto. 814/01, art. 2, inc. a).'),
+        ('5', 'Servicios eventuales. Dto. 814/01, art. 2, inc. a).'),
+        ('6', 'Dto. 814/01, art. 2, inc. b). Provincias. Ley 22.016.'),
+        ('7', 'Colegios privados.')
     ], string='AFIP SICOSS - Tipo Empleador', default="1",
         help="Para seleccionar este campo correctamente, verificar en sus declaraciones 931 anteriores.")
