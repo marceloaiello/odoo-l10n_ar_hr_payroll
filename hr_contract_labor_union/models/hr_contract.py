39631f24ca299a583824782b0695b4a7f3e9dfca
nicolasrsande <nicolasrsande@gmail.com>
2021-06-27 15:54:48 -0300

    merge

--- hr_contract_labor_union/models/hr_contract.py
+++ hr_contract_labor_union/models/hr_contract.py
@@ -6,7 +6,7 @@ from odoo import api, fields, models
 
 class HrContract(models.Model):
     _inherit = 'hr.contract'
-    
+
     aplica_cct = fields.Boolean(string='Aplica C.C.T / Convenio?')
     cct_id = fields.Many2one(comodel_name='hr.labor_union', string='C.C.T / Sindicato')
     cct_category_id = fields.Many2one(
