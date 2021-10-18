411cc31a76d0ff1ae11e0cec0788901f86d0c63a
nicolasrsande <nicolasrsande@gmail.com>
2021-10-18 13:27:19 -0300

    merge new changes

--- l10n_ar_payroll/__manifest__.py
+++ l10n_ar_payroll/__manifest__.py
@@ -27,6 +27,7 @@
         'data/hr_leaves.xml',
         'data/hr_salary_rule_category.xml',
         'data/hr_salary_rule.xml',
+        'data/hr_payroll_structure.xml',
     ],
     'installable': True,
     'auto_install': False,
