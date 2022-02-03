32b145d1237e1f6073673bd4b363a1b6a1d1d2ed
nicolasrsande <nicolasrsande@gmail.com>
2022-02-03 11:49:46 -0300

    merge new changes

--- l10n_ar_payroll_sicoss_export/models/payroll_sicoss_entry_item.py
+++ l10n_ar_payroll_sicoss_export/models/payroll_sicoss_entry_item.py
@@ -9,10 +9,13 @@ class PayrollSicossEntryItem(models.Model):
     _name = 'payroll.sicoss_entry.item'
     _description = 'Payroll Sicoss Entry Item'
 
-    sicoss_entry_id = fields.Many2one('payroll.sicoss_entry', string='Liquidacion AFIP-SICOSS')
+    sicoss_entry_id = fields.Many2one(
+        'payroll.sicoss_entry', string='Liquidacion AFIP-SICOSS')
     employee_id = fields.Many2one('hr.employee', string='Empleado')
-    worked_days_count = fields.Integer(string="# Dias Trabajados", default=0.00, compute='_compute_worked_days_count')
-    horas_extra_count = fields.Integer(string="# Horas Extra", default=0.00, compute='_compute_horas_extra_count')
+    worked_days_count = fields.Integer(
+        string="# Dias Trabajados", default=0.00, compute='_compute_worked_days_count')
+    horas_extra_count = fields.Integer(
+        string="# Horas Extra", default=0.00, compute='_compute_horas_extra_count')
     sueldo = fields.Float(string='Sueldo')
     adicionales = fields.Float(string='Adicionales')
     horas_extra = fields.Float(string='Horas Extra')
@@ -28,18 +31,32 @@ class PayrollSicossEntryItem(models.Model):
     subtotal_no_remunerativo = fields.Float(string='Subtotal No Remunerativo')
     subtotal_descuentos = fields.Float(string='Subtotal Descuentos')
     subtotal_neto = fields.Float(string='Subtotal Neto')
-    importe_detraccion = fields.Float(compute='_compute_importe_detraccion', string='Importe Detraccion')
-    rem_1 = fields.Float('Remuneracion 1', readonly=True, default=0.01, compute='_compute_rems')  # Base aportes SIPA
-    rem_2 = fields.Float('Remuneracion 2', readonly=True, default=0.01, compute='_compute_rems')  # Base a contribuciones SIPA e INSJP
-    rem_3 = fields.Float('Remuneracion 3', readonly=True, default=0.01, compute='_compute_rems')  # Base contribuciones Fondo Nacional De Empleo/Renatre y Asignaciones
-    rem_4 = fields.Float('Remuneracion 4', readonly=True, default=0.01, compute='_compute_rems')  # Base aportes obra social y ANSSAL
-    rem_5 = fields.Float('Remuneracion 5', readonly=True, default=0.01, compute='_compute_rems')  # Base aportes INSJP
-    rem_6 = fields.Float('Remuneracion 6', readonly=True, default=0.01, compute='_compute_rems')  # No modifica
-    rem_7 = fields.Float('Remuneracion 7', readonly=True, default=0.01, compute='_compute_rems')   # No modifica
-    rem_8 = fields.Float('Remuneracion 8', readonly=True, default=0.01, compute='_compute_rems')  # Base contribuciones obra social y ANSSAL
-    rem_9 = fields.Float('Remuneracion 9', readonly=True, default=0.01, compute='_compute_rems')   # Base LRT (ART)
-    rem_10 = fields.Float('Remuneracion 10', readonly=True, default=0.01, compute='_compute_rems')  # Base contribuciones seguridad social con detraccion
-    rem_11 = fields.Float('Remuneracion 11', readonly=True, default=0.01, compute='_compute_rems')  # TODO: Ver que es
+    importe_detraccion = fields.Float(
+        compute='_compute_importe_detraccion', string='Importe Detraccion')
+    rem_1 = fields.Float('Remuneracion 1', readonly=True, default=0.01,
+                         compute='_compute_rems')  # Base aportes SIPA
+    rem_2 = fields.Float('Remuneracion 2', readonly=True, default=0.01,
+                         compute='_compute_rems')  # Base a contribuciones SIPA e INSJP
+    # Base contribuciones Fondo Nacional De Empleo/Renatre y Asignaciones
+    rem_3 = fields.Float('Remuneracion 3', readonly=True,
+                         default=0.01, compute='_compute_rems')
+    rem_4 = fields.Float('Remuneracion 4', readonly=True, default=0.01,
+                         compute='_compute_rems')  # Base aportes obra social y ANSSAL
+    rem_5 = fields.Float('Remuneracion 5', readonly=True, default=0.01,
+                         compute='_compute_rems')  # Base aportes INSJP
+    rem_6 = fields.Float('Remuneracion 6', readonly=True,
+                         default=0.01, compute='_compute_rems')  # No modifica
+    rem_7 = fields.Float('Remuneracion 7', readonly=True,
+                         default=0.01, compute='_compute_rems')   # No modifica
+    rem_8 = fields.Float('Remuneracion 8', readonly=True, default=0.01,
+                         compute='_compute_rems')  # Base contribuciones obra social y ANSSAL
+    rem_9 = fields.Float('Remuneracion 9', readonly=True, default=0.01,
+                         compute='_compute_rems')   # Base LRT (ART)
+    # Base contribuciones seguridad social con detraccion
+    rem_10 = fields.Float('Remuneracion 10', readonly=True,
+                          default=0.01, compute='_compute_rems')
+    rem_11 = fields.Float('Remuneracion 11', readonly=True,
+                          default=0.01, compute='_compute_rems')  # TODO: Ver que es
 
     @api.depends('employee_id', 'horas_extra')
     def _compute_horas_extra_count(self):
@@ -61,7 +78,8 @@ class PayrollSicossEntryItem(models.Model):
     @api.depends('subtotal_bruto', 'subtotal_no_remunerativo', 'importe_detraccion')
     def _compute_rems(self):
         for record in self:
-            if not (record.employee_id.struct_id.name in 'NO APORT. Y CONTRIB.'):  # TODO: Deberia agregar un boolean en la structure para setear esto.
+            # TODO: Deberia agregar un boolean en la structure para setear esto.
+            if not (record.employee_id.struct_id.name in 'NO APORT. Y CONTRIB.'):
                 record.rem_1 = record.subtotal_bruto
                 record.rem_2 = record.subtotal_bruto
                 record.rem_3 = record.subtotal_bruto
@@ -71,8 +89,10 @@ class PayrollSicossEntryItem(models.Model):
                 record.rem_8 = record.subtotal_bruto
                 record.rem_9 = record.subtotal_bruto + record.subtotal_no_remunerativo
                 record.rem_10 = record.subtotal_bruto - record.importe_detraccion
-            else:  # Caso para empleados fuera de convenio que no liquidan aportes y contribuciones (gerentes)
-                record.rem_9 = record.subtotal_bruto + record.subtotal_no_remunerativo  # Solamente liquidan ART.
+            # Caso para empleados fuera de convenio que no liquidan aportes y contribuciones (gerentes)
+            else:
+                # Solamente liquidan ART.
+                record.rem_9 = record.subtotal_bruto + record.subtotal_no_remunerativo
 
     @api.model
     def _compute_sicoss_entry_item(self, item):
@@ -105,25 +125,28 @@ class PayrollSicossEntryItem(models.Model):
             'codigo_siniestrado': contract.afip_codigo_siniestrado_id.afip_id,
             'corresponde_reduccion': 'N',
             'capital_recomp_lrt': 0,
-            'tipo_empresa': 0, # TODO: Hacer esto
+            'tipo_empresa': 0,  # TODO: Hacer esto
             'aporte_adicional_os': 0,
-            'regimen': 0, # TODO: Hacer esto
-            'sit_revista_1': contract.afip_situacion_revista_id.afip_id, # TODO: Ver varias situaciones de revista
-            'dia_sit_revista_1': 0, # TODO: Ver varias situaciones de revista
-            'sit_revista_2': contract.afip_situacion_revista_id.afip_id, # TODO: Ver varias situaciones de revista
-            'dia_sit_revista_2': 0, # TODO: Ver varias situaciones de revista
-            'sit_revista_3': contract.afip_situacion_revista_id.afip_id, # TODO: Ver varias situaciones de revista
-            'dia_sit_revista_3': 0, # TODO: Ver varias situaciones de revista
+            'regimen': 0,  # TODO: Hacer esto
+            # TODO: Ver varias situaciones de revista
+            'sit_revista_1': contract.afip_situacion_revista_id.afip_id,
+            'dia_sit_revista_1': 0,  # TODO: Ver varias situaciones de revista
+            # TODO: Ver varias situaciones de revista
+            'sit_revista_2': contract.afip_situacion_revista_id.afip_id,
+            'dia_sit_revista_2': 0,  # TODO: Ver varias situaciones de revista
+            # TODO: Ver varias situaciones de revista
+            'sit_revista_3': contract.afip_situacion_revista_id.afip_id,
+            'dia_sit_revista_3': 0,  # TODO: Ver varias situaciones de revista
             'sueldo_adiconales': item.sueldo,
             'sac': item.sac,
             'horas_extra': item.horas_extra,
             'zona_desfavorable': item.plus_zona_desfavorable,
             'vacaciones': item.vacaciones,
-            'dias_trabajados': 30, # TODO: Ver como cambiar esto
+            'dias_trabajados': 30,  # TODO: Ver como cambiar esto
             'rem_5': item.rem_5,
             'aplica_convenio': contract.aplica_cct,
             'rem_6': item.rem_6,
-            'tipo_operacion': 0, # TODO: Ver que es esto
+            'tipo_operacion': 0,  # TODO: Ver que es esto
             'adicionales': item.adicionales,
             'premios': item.premios,
             'rem_7': item.rem_7,
@@ -139,6 +162,5 @@ class PayrollSicossEntryItem(models.Model):
             'incremento_salarial': item.incremento_salarial,
             'rem_11': item.rem_11,
         }
-
 
         return res
